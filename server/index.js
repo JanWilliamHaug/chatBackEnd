aa736fbd3dfcfd7ee48f85b1c44ed6ea2f70745c
JanWilliamHaug <71844869+JanWilliamHaug@users.noreply.github.com>
2023-05-08 00:31:53 -0700

    eited index.js

--- server/index.js
+++ server/index.js
@@ -1,25 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const app = require('./server/server');
 const dotenv = require('dotenv');
 
 dotenv.config();
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-const chatbotRouter = require('./routes/chatbot');
-app.use('/chatbot', chatbotRouter);
-
-
-const uri = process.env.MONGODB_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log('MongoDB connection established successfully');
-});
 
 const PORT = process.env.PORT || 3000;
 
