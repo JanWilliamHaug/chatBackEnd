f3d687c71cd02604d98021d57a7b3f7147daa2cb
JanWilliamHaug <71844869+JanWilliamHaug@users.noreply.github.com>
2023-05-08 00:35:18 -0700

    changed index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 const app = require('./server/server');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
+
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB connection established successfully');
+});
 
 const PORT = process.env.PORT || 3000;
 
